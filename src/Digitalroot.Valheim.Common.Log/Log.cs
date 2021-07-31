4914cfdba9f8c54059a3509cb218b23b6274fbde
Nicholas Dunnaway <nick@dunnaway.org>
2021-07-30 20:43:41 -0700

    Breaking Changes to Logging.

--- src/Digitalroot.Valheim.Common.Log/Log.cs
+++ src/Digitalroot.Valheim.Common.Log/Log.cs
@@ -1,12 +1,7 @@
-﻿using BepInEx;
-using BepInEx.Logging;
+﻿using BepInEx.Logging;
 using JetBrains.Annotations;
-using Digitalroot.Valheim.Common.Json;
 using System;
-using System.IO;
-using System.Reflection;
-using System.Text;
-using System.Timers;
+using System.Collections.Generic;
 
 namespace Digitalroot.Valheim.Common
 {
@@ -15,9 +10,7 @@ namespace Digitalroot.Valheim.Common
   /// </summary>
   public sealed class Log : IDisposable
   {
-    private ManualLogSource _loggerRef;
-    private string _source = nameof(Digitalroot);
-    private const int FlushTick = 300;
+    private static readonly Dictionary<string, TraceLogger> TraceLoggers = new Dictionary<string, TraceLogger>();
 
     private static Log Instance { get; } = new Log();
 
@@ -27,245 +20,134 @@ namespace Digitalroot.Valheim.Common
 
     private Log()
     {
-      Init();
+      // Create Default TraceLogger
+      TraceLoggers.Add(nameof(Digitalroot), new TraceLogger(nameof(Digitalroot), true));
     }
 
+    #region Implementation of IDisposable
+
+    /// <inheritdoc />
     public void Dispose()
     {
-      _timer.Stop();
-      _timer.Dispose();
-      _streamWriter.Flush();
-      _fileStream.Flush();
-      _streamWriter.Close();
-      _fileStream.Close();
-      _streamWriter.Dispose();
-      _fileStream.Dispose();
-    }
-
-    private void Init()
-    {
-      if (_timer != null)
+      foreach (var traceLogger in TraceLoggers.Values)
       {
-        StopTrace();
-        _timer.Dispose();
-        _streamWriter.Flush();
-        _fileStream.Flush();
-        _streamWriter.Close();
-        _fileStream.Close();
-        _streamWriter.Dispose();
-        _fileStream.Dispose();
-      }
-
-      _loggerRef = Logger.CreateLogSource(_source);
-      _traceFileInfo = new FileInfo(Path.Combine(Paths.BepInExRootPath ?? AssemblyDirectory.FullName, $"{_source}.Trace.log"));
-
-      if (_traceFileInfo.Exists)
-      {
-        _traceFileInfo.Delete();
-        _traceFileInfo.Refresh();
-      }
-
-      if (IsTraceEnabled)
-      {
-        _timer = new System.Timers.Timer(FlushTick)
-        {
-          AutoReset = true,
-          Enabled = true
-        };
-        _timer.Elapsed += TimerElapsed;
-
-        StartTrace();
-      }
-    }
-
-    public static void SetSource(string source)
-    {
-      Instance._source = source;
-      Instance.Init();
-    }
-    
-    #region Trace
-
-    // ReSharper disable once MemberCanBePrivate.Global
-    public static bool IsTraceEnabled { get; private set; }
-
-    private static readonly object FileLock = new object();
-    private FileInfo _traceFileInfo;
-    private FileStream _fileStream;
-    private StreamWriter _streamWriter;
-    private System.Timers.Timer _timer;
-
-    private void StartTrace()
-    {
-      lock (FileLock)
-      {
-        if (_fileStream != null && !_fileStream.CanWrite)
-        {
-          _fileStream = null;
-          _streamWriter = null;
-          _timer = null;
-        }
-
-        if (_fileStream == null) _fileStream = new FileStream(_traceFileInfo.FullName, FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite, 4096, FileOptions.WriteThrough);
-        if (_streamWriter == null) _streamWriter = new StreamWriter(_fileStream, Encoding.UTF8, 4096);
-
-        if (_timer == null)
-        {
-          _timer = new System.Timers.Timer(FlushTick);
-        }
-
-        _timer.Start();
-        _loggerRef.LogEvent += OnLogEvent;
-      }
-    }
-
-    private void StopTrace()
-    {
-      _timer.Stop();
-      _loggerRef.LogEvent -= OnLogEvent;
-    }
-
-    private void OnLogEvent(object sender, LogEventArgs e)
-    {
-      if (e.Source.SourceName == _source || e.Level == (LogLevel.Error | LogLevel.Fatal))
-      {
-        lock (FileLock)
-        {
-          if (e.Data is string)
-          {
-            _streamWriter.WriteLine($"[{e.Level,-7}:{e.Source.SourceName,10}] {e.Data}");
-          }
-          else
-          {
-            _streamWriter.WriteLine($"[{e.Level,-7}:{e.Source.SourceName,10}] {JsonSerializationProvider.ToJson(e.Data)}");
-          }
-        }
-      }
-    }
-
-    private void TimerElapsed(object sender, ElapsedEventArgs e)
-    {
-      lock (FileLock)
-      {
-        _streamWriter?.Flush();
-      }
-    }
-
-    [UsedImplicitly]
-    public static void EnableTrace()
-    {
-      Instance.StartTrace();
-      IsTraceEnabled = true;
-    }
-
-    [UsedImplicitly]
-    public static void DisableTrace()
-    {
-      Instance.StopTrace();
-      IsTraceEnabled = false;
-    }
-
-    public static void ToggleTrace(bool value)
-    {
-      switch (value)
-      {
-        case true:
-          EnableTrace();
-          break;
-        default:
-          DisableTrace();
-          break;
+        traceLogger.Dispose();
       }
     }
 
     #endregion
 
+    public static void FlushLog(ITraceableLogging sender)
+    {
+      if (TraceLoggers.ContainsKey(sender.Source))
+      {
+        TraceLoggers[sender.Source].Flush();
+      }
+    }
+
+    public static void FlushAllLogs()
+    {
+      foreach (var traceLogger in TraceLoggers.Values)
+      {
+        traceLogger.Flush();
+      }
+    }
+
+    public static void RegisterSource(ITraceableLogging sender, bool enabledTrace)
+    {
+      if (TraceLoggers.ContainsKey(sender.Source)) return;
+      TraceLoggers.Add(sender.Source, new TraceLogger(sender.Source, enabledTrace));
+    }
+    
     #region Logging
 
     [UsedImplicitly]
-    public static void Fatal(Exception e, int i = 1)
+    public static void Fatal(ITraceableLogging sender, Exception e, int i = 1)
     {
-      Fatal($"Message: {e.Message}");
-      Fatal($"StackTrace: {e.StackTrace}");
-      Fatal($"Source: {e.Source}");
+      Fatal(sender, $"Message: {e.Message}");
+      Fatal(sender, $"StackTrace: {e.StackTrace}");
+      Fatal(sender, $"Source: {e.Source}");
       if (e.Data.Count > 0)
       {
         foreach (var key in e.Data.Keys)
         {
-          Fatal($"key: {key}, value: {e.Data[key]}");
+          Fatal(sender, $"key: {key}, value: {e.Data[key]}");
         }
       }
 
       if (e.InnerException != null)
       {
-        Fatal($"--- InnerException [{i}][Start] ---");
-        Fatal(e.InnerException, ++i);
+        Fatal(sender, $"--- InnerException [{i}][Start] ---");
+        Fatal(sender, e.InnerException, ++i);
       }
     }
 
-    [UsedImplicitly]
-    public static void Fatal(object value)
+    private static TraceLogger GetTraceLogger(ITraceableLogging sender)
     {
-      Instance._loggerRef.LogFatal(value);
+      return TraceLoggers.ContainsKey(sender.Source) ? TraceLoggers[sender.Source] : TraceLoggers[nameof(Digitalroot)];
     }
 
     [UsedImplicitly]
-    public static void Error(Exception e, int i = 1)
+    public static void Fatal(ITraceableLogging sender, object value)
     {
-      Error($"Message: {e.Message}");
-      Error($"StackTrace: {e.StackTrace}");
-      Error($"Source: {e.Source}");
+      GetTraceLogger(sender).LoggerRef.LogFatal(value);
+    }
+
+    [UsedImplicitly]
+    public static void Error(ITraceableLogging sender, Exception e, int i = 1)
+    {
+      Error(sender, $"Message: {e.Message}");
+      Error(sender, $"StackTrace: {e.StackTrace}");
+      Error(sender, $"Source: {e.Source}");
       if (e.Data.Count > 0)
       {
         foreach (var key in e.Data.Keys)
         {
-          Error($"key: {key}, value: {e.Data[key]}");
+          Error(sender, $"key: {key}, value: {e.Data[key]}");
         }
       }
 
-      if (e.InnerException != null)
+      if (e.InnerException == null) return;
+      Error(sender, $"--- InnerException [{i}][Start] ---");
+      Error(sender, e.InnerException, ++i);
+    }
+
+    [UsedImplicitly]
+    public static void Error(ITraceableLogging sender, object value)
+    {
+      GetTraceLogger(sender).LoggerRef.LogError(value);
+    }
+
+    [UsedImplicitly]
+    public static void Warning(ITraceableLogging sender, object value)
+    {
+      GetTraceLogger(sender).LoggerRef.LogWarning(value);
+    }
+
+    [UsedImplicitly]
+    public static void Message(ITraceableLogging sender, object value)
+    {
+      GetTraceLogger(sender).LoggerRef.LogMessage(value);
+    }
+
+    [UsedImplicitly]
+    public static void Info(ITraceableLogging sender, object value)
+    {
+      GetTraceLogger(sender).LoggerRef.LogInfo(value);
+    }
+
+    [UsedImplicitly]
+    public static void Debug(ITraceableLogging sender, object value)
+    {
+      GetTraceLogger(sender).LoggerRef.LogDebug(value);
+    }
+
+    [UsedImplicitly]
+    public static void Trace(ITraceableLogging sender, object value)
+    {
+      if (GetTraceLogger(sender).IsTraceEnabled)
       {
-        Error($"--- InnerException [{i}][Start] ---");
-        Error(e.InnerException, ++i);
-      }
-    }
-
-    [UsedImplicitly]
-    public static void Error(object value)
-    {
-      Instance._loggerRef.LogError(value);
-    }
-
-    [UsedImplicitly]
-    public static void Warning(object value)
-    {
-      Instance._loggerRef.LogWarning(value);
-    }
-
-    [UsedImplicitly]
-    public static void Message(object value)
-    {
-      Instance._loggerRef.LogMessage(value);
-    }
-
-    [UsedImplicitly]
-    public static void Info(object value)
-    {
-      Instance._loggerRef.LogInfo(value);
-    }
-
-    [UsedImplicitly]
-    public static void Debug(object value)
-    {
-      Instance._loggerRef.LogDebug(value);
-    }
-
-    [UsedImplicitly]
-    public static void Trace(object value)
-    {
-      if (IsTraceEnabled)
-      {
-        Instance._loggerRef.Log(LogLevel.All, value);
+        GetTraceLogger(sender).LoggerRef.Log(LogLevel.All, value);
       }
     }
 
@@ -274,17 +156,6 @@ namespace Digitalroot.Valheim.Common
     public static void OnDestroy()
     {
       Instance.Dispose();
-    }
-
-    private static DirectoryInfo AssemblyDirectory
-    {
-      get
-      {
-        string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-        UriBuilder uri = new UriBuilder(codeBase);
-        var fileInfo = new FileInfo(Uri.UnescapeDataString(uri.Path));
-        return fileInfo.Directory;
-      }
     }
   }
 }
--- src/UnitTests/LoggingTests.cs
+++ src/UnitTests/LoggingTests.cs
@@ -1,5 +1,7 @@
 using Digitalroot.Valheim.Common;
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
@@ -7,30 +9,51 @@ namespace UnitTests
 {
   public class LoggingTests
   {
-    [SetUp]
-    public void Setup()
+    private class TestLoggerClass : ITraceableLogging
     {
+      public TestLoggerClass()
+      : this(nameof(LogTest))
+      {
+      }
+
+      public TestLoggerClass(string source)
+      {
+        Source = source;
+      }
+
+      #region Implementation of ITraceableLogging
+
+      /// <inheritdoc />
+      public string Source { get; private set; }
+
+      #endregion
     }
 
-    [Test(Author = "Digitalroot", Description ="Tests logging with the default source.", TestOf = typeof(Log)), Timeout(500), Order(1)]
+    [Test(Author = "Digitalroot", Description = "Tests logging with the default source.", TestOf = typeof(Log)), Timeout(500)]
     public void LogTest()
     {
-      Log.EnableTrace();
-      Log.Info(nameof(Log.Info));
-      Log.Debug(nameof(Log.Debug));
-      Log.Message(nameof(Log.Message));
-      Log.Error(nameof(Log.Error));
-      Log.Fatal(nameof(Log.Fatal));
-      Log.Warning(nameof(Log.Warning));
-      Log.Trace(nameof(Log.Trace));
-      Log.OnDestroy();
+      var logger = new TestLoggerClass();
+
+      Log.Info(logger, nameof(Log.Info));
+      Log.Debug(logger, nameof(Log.Debug));
+      Log.Message(logger, nameof(Log.Message));
+      Log.Error(logger, nameof(Log.Error));
+      Log.Fatal(logger, nameof(Log.Fatal));
+      Log.Warning(logger, nameof(Log.Warning));
+      Log.Trace(logger, nameof(Log.Trace));
+      Log.FlushLog(logger);
 
       FileInfo logFileInfo = new FileInfo("Digitalroot.Trace.log");
+      System.Console.WriteLine(logFileInfo.FullName);
       Assert.That(logFileInfo, Is.Not.Null);
       Assert.That(logFileInfo.Exists, Is.True);
-      foreach (var line in File.ReadLines(logFileInfo.FullName, Encoding.UTF8))
+      using var fileStream = new FileStream(logFileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 1024, FileOptions.None);
+      using var sr = new StreamReader(fileStream, Encoding.UTF8);
+
+      while (!sr.EndOfStream)
       {
-        Assert.That(line, 
+        string line = sr.ReadLine();
+        Assert.That(line,
           Is.EqualTo("[Info   :Digitalroot] Info")
             .Or.EqualTo("[Debug  :Digitalroot] Debug")
             .Or.EqualTo("[Message:Digitalroot] Message")
@@ -38,37 +61,268 @@ namespace UnitTests
             .Or.EqualTo("[Fatal  :Digitalroot] Fatal")
             .Or.EqualTo("[Warning:Digitalroot] Warning")
             .Or.EqualTo("[All    :Digitalroot] Trace")
-          );
+        );
       }
     }
 
-    [Test(Author = "Digitalroot", Description = "Tests logging with a custom source.", TestOf = typeof(Log)), Timeout(500), Order(2)]
+    [Test(Author = "Digitalroot", Description = "Tests logging with a custom source.", TestOf = typeof(Log)), Timeout(500)]
     public void ChangeSourceTest()
     {
-      Log.EnableTrace();
-      Log.SetSource(nameof(ChangeSourceTest));
-      Log.Info(nameof(Log.Info));
-      Log.Debug(nameof(Log.Debug));
-      Log.Message(nameof(Log.Message));
-      Log.Error(nameof(Log.Error));
-      Log.Fatal(nameof(Log.Fatal));
-      Log.Warning(nameof(Log.Warning));
-      Log.Trace(nameof(Log.Trace));
-      Log.OnDestroy();
+      var logger = new TestLoggerClass();
+      Log.RegisterSource(logger, true);
+      Log.Info(logger, nameof(Log.Info));
+      Log.Debug(logger, nameof(Log.Debug));
+      Log.Message(logger, nameof(Log.Message));
+      Log.Error(logger, nameof(Log.Error));
+      Log.Fatal(logger, nameof(Log.Fatal));
+      Log.Warning(logger, nameof(Log.Warning));
+      Log.Trace(logger, nameof(Log.Trace));
+      Log.FlushLog(logger);
 
-      FileInfo logFileInfo = new FileInfo("ChangeSourceTest.Trace.log");
+      FileInfo logFileInfo = new FileInfo("LogTest.Trace.log");
+      System.Console.WriteLine(logFileInfo.FullName);
       Assert.That(logFileInfo, Is.Not.Null);
       Assert.That(logFileInfo.Exists, Is.True);
-      foreach (var line in File.ReadLines(logFileInfo.FullName, Encoding.UTF8))
+
+      using var fileStream = new FileStream(logFileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 1024, FileOptions.None);
+      using var sr = new StreamReader(fileStream, Encoding.UTF8);
+
+      while (!sr.EndOfStream)
       {
+        string line = sr.ReadLine();
         Assert.That(line,
-          Is.EqualTo("[Info   :ChangeSourceTest] Info")
-            .Or.EqualTo("[Debug  :ChangeSourceTest] Debug")
-            .Or.EqualTo("[Message:ChangeSourceTest] Message")
-            .Or.EqualTo("[Error  :ChangeSourceTest] Error")
-            .Or.EqualTo("[Fatal  :ChangeSourceTest] Fatal")
-            .Or.EqualTo("[Warning:ChangeSourceTest] Warning")
-            .Or.EqualTo("[All    :ChangeSourceTest] Trace")
+          Is.EqualTo("[Info   :   LogTest] Info")
+            .Or.EqualTo("[Debug  :   LogTest] Debug")
+            .Or.EqualTo("[Message:   LogTest] Message")
+            .Or.EqualTo("[Error  :   LogTest] Error")
+            .Or.EqualTo("[Fatal  :   LogTest] Fatal")
+            .Or.EqualTo("[Warning:   LogTest] Warning")
+            .Or.EqualTo("[All    :   LogTest] Trace")
+        );
+      }
+    }
+
+    [Test(Author = "Digitalroot", Description = "Tests logging with a custom source.", TestOf = typeof(Log)), Timeout(500)]
+    public void MultiSourceTest()
+    {
+      var loggerA = new TestLoggerClass("loggerA");
+      Log.RegisterSource(loggerA, true);
+      Log.Info(loggerA, nameof(Log.Info));
+      Log.Debug(loggerA, nameof(Log.Debug));
+      Log.Message(loggerA, nameof(Log.Message));
+      Log.Error(loggerA, nameof(Log.Error));
+      Log.Fatal(loggerA, nameof(Log.Fatal));
+      Log.Warning(loggerA, nameof(Log.Warning));
+      Log.Trace(loggerA, nameof(Log.Trace));
+      Log.FlushLog(loggerA);
+
+      var loggerB = new TestLoggerClass("loggerB");
+      Log.RegisterSource(loggerB, true);
+      Log.Info(loggerB, nameof(Log.Info));
+      Log.Debug(loggerB, nameof(Log.Debug));
+      Log.Message(loggerB, nameof(Log.Message));
+      Log.Error(loggerB, nameof(Log.Error));
+      Log.Fatal(loggerB, nameof(Log.Fatal));
+      Log.Warning(loggerB, nameof(Log.Warning));
+      Log.Trace(loggerB, nameof(Log.Trace));
+      Log.FlushLog(loggerB);
+
+      var loggerC = new TestLoggerClass("loggerC");
+      Log.RegisterSource(loggerC, true);
+      Log.Info(loggerC, nameof(Log.Info));
+      Log.Debug(loggerC, nameof(Log.Debug));
+      Log.Message(loggerC, nameof(Log.Message));
+      Log.Error(loggerC, nameof(Log.Error));
+      Log.Fatal(loggerC, nameof(Log.Fatal));
+      Log.Warning(loggerC, nameof(Log.Warning));
+      Log.Trace(loggerC, nameof(Log.Trace));
+      Log.FlushLog(loggerC);
+
+      FileInfo logFileInfoA = new FileInfo("loggerA.Trace.log");
+      System.Console.WriteLine(logFileInfoA.FullName);
+      Assert.That(logFileInfoA, Is.Not.Null);
+      Assert.That(logFileInfoA.Exists, Is.True);
+
+      using var fileStreamA = new FileStream(logFileInfoA.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 1024, FileOptions.None);
+      using var streamReaderA = new StreamReader(fileStreamA, Encoding.UTF8);
+
+      while (!streamReaderA.EndOfStream)
+      {
+        string line = streamReaderA.ReadLine();
+        Assert.That(line,
+          Is.EqualTo("[Info   :   loggerA] Info")
+            .Or.EqualTo("[Debug  :   loggerA] Debug")
+            .Or.EqualTo("[Message:   loggerA] Message")
+            .Or.EqualTo("[Error  :   loggerA] Error")
+            .Or.EqualTo("[Fatal  :   loggerA] Fatal")
+            .Or.EqualTo("[Warning:   loggerA] Warning")
+            .Or.EqualTo("[All    :   loggerA] Trace")
+        );
+      }
+
+      FileInfo logFileInfoB = new FileInfo("loggerB.Trace.log");
+      System.Console.WriteLine(logFileInfoB.FullName);
+      Assert.That(logFileInfoB, Is.Not.Null);
+      Assert.That(logFileInfoB.Exists, Is.True);
+      
+      using var fileStreamB = new FileStream(logFileInfoB.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 1024, FileOptions.None);
+      using var streamReaderB = new StreamReader(fileStreamB, Encoding.UTF8);
+      
+      while (!streamReaderB.EndOfStream)
+      {
+        string line = streamReaderB.ReadLine();
+        Assert.That(line,
+          Is.EqualTo("[Info   :   loggerB] Info")
+            .Or.EqualTo("[Debug  :   loggerB] Debug")
+            .Or.EqualTo("[Message:   loggerB] Message")
+            .Or.EqualTo("[Error  :   loggerB] Error")
+            .Or.EqualTo("[Fatal  :   loggerB] Fatal")
+            .Or.EqualTo("[Warning:   loggerB] Warning")
+            .Or.EqualTo("[All    :   loggerB] Trace")
+        );
+      }
+
+      FileInfo logFileInfoC = new FileInfo("loggerC.Trace.log");
+      System.Console.WriteLine(logFileInfoC.FullName);
+      Assert.That(logFileInfoC, Is.Not.Null);
+      Assert.That(logFileInfoC.Exists, Is.True);
+
+      using var fileStreamC = new FileStream(logFileInfoC.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 1024, FileOptions.None);
+      using var streamReaderC = new StreamReader(fileStreamC, Encoding.UTF8);
+
+      while (!streamReaderC.EndOfStream)
+      {
+        string line = streamReaderC.ReadLine();
+        Assert.That(line,
+          Is.EqualTo("[Info   :   loggerC] Info")
+            .Or.EqualTo("[Debug  :   loggerC] Debug")
+            .Or.EqualTo("[Message:   loggerC] Message")
+            .Or.EqualTo("[Error  :   loggerC] Error")
+            .Or.EqualTo("[Fatal  :   loggerC] Fatal")
+            .Or.EqualTo("[Warning:   loggerC] Warning")
+            .Or.EqualTo("[All    :   loggerC] Trace")
+        );
+      }
+    }
+
+    [Test(Author = "Digitalroot", Description = "Tests logging with a custom source.", TestOf = typeof(Log)), Timeout(500)]
+    public void MultiSourceWeaveTest()
+    {
+      var loggerA = new TestLoggerClass("loggerA");
+      Log.RegisterSource(loggerA, true);
+      var loggerB = new TestLoggerClass("loggerB");
+      Log.RegisterSource(loggerB, true);
+      var loggerC = new TestLoggerClass("loggerC");
+      Log.RegisterSource(loggerC, true);
+
+      var loggers = new List<ITraceableLogging> {loggerA, loggerB, loggerC};
+      Random rnd = new Random();
+      for (int i = 0; i < 1000; i++)
+      {
+        Log.Info(loggers[rnd.Next(0, 3)], nameof(Log.Info));
+        Log.Info(loggers[rnd.Next(0, 3)], nameof(Log.Info));
+        Log.Info(loggers[rnd.Next(0, 3)], nameof(Log.Info));
+        Log.Debug(loggers[rnd.Next(0, 3)], nameof(Log.Debug));
+        Log.Debug(loggers[rnd.Next(0, 3)], nameof(Log.Debug));
+        Log.Debug(loggers[rnd.Next(0, 3)], nameof(Log.Debug));
+        Log.Message(loggers[rnd.Next(0, 3)], nameof(Log.Message));
+        Log.Message(loggers[rnd.Next(0, 3)], nameof(Log.Message));
+        Log.Message(loggers[rnd.Next(0, 3)], nameof(Log.Message));
+        Log.Error(loggers[rnd.Next(0, 3)], nameof(Log.Error));
+        Log.Error(loggers[rnd.Next(0, 3)], nameof(Log.Error));
+        Log.Error(loggers[rnd.Next(0, 3)], nameof(Log.Error));
+        Log.Fatal(loggers[rnd.Next(0, 3)], nameof(Log.Fatal));
+        Log.Fatal(loggers[rnd.Next(0, 3)], nameof(Log.Fatal));
+        Log.Fatal(loggers[rnd.Next(0, 3)], nameof(Log.Fatal));
+        Log.Warning(loggers[rnd.Next(0, 3)], nameof(Log.Warning));
+        Log.Warning(loggers[rnd.Next(0, 3)], nameof(Log.Warning));
+        Log.Warning(loggers[rnd.Next(0, 3)], nameof(Log.Warning));
+        Log.Trace(loggers[rnd.Next(0, 3)], nameof(Log.Trace));
+        Log.Trace(loggers[rnd.Next(0, 3)], nameof(Log.Trace));
+        Log.Trace(loggers[rnd.Next(0, 3)], nameof(Log.Trace));
+        Log.Info(loggers[rnd.Next(0, 3)], nameof(Log.Info));
+        Log.Debug(loggers[rnd.Next(0, 3)], nameof(Log.Debug));
+        Log.Message(loggers[rnd.Next(0, 3)], nameof(Log.Message));
+        Log.Error(loggers[rnd.Next(0, 3)], nameof(Log.Error));
+        Log.Fatal(loggers[rnd.Next(0, 3)], nameof(Log.Fatal));
+        Log.Warning(loggers[rnd.Next(0, 3)], nameof(Log.Warning));
+        Log.Trace(loggers[rnd.Next(0, 3)], nameof(Log.Trace));
+        Log.Info(loggers[rnd.Next(0, 3)], nameof(Log.Info));
+        Log.Debug(loggers[rnd.Next(0, 3)], nameof(Log.Debug));
+        Log.Message(loggers[rnd.Next(0, 3)], nameof(Log.Message));
+        Log.Error(loggers[rnd.Next(0, 3)], nameof(Log.Error));
+        Log.Fatal(loggers[rnd.Next(0, 3)], nameof(Log.Fatal));
+        Log.Warning(loggers[rnd.Next(0, 3)], nameof(Log.Warning));
+        Log.Trace(loggers[rnd.Next(0, 3)], nameof(Log.Trace));
+      }
+
+      Log.FlushLog(loggerA);
+      Log.FlushLog(loggerB);
+      Log.FlushLog(loggerC);
+
+      FileInfo logFileInfoA = new FileInfo("loggerA.Trace.log");
+      System.Console.WriteLine(logFileInfoA.FullName);
+      Assert.That(logFileInfoA, Is.Not.Null);
+      Assert.That(logFileInfoA.Exists, Is.True);
+
+      using var fileStreamA = new FileStream(logFileInfoA.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 1024, FileOptions.None);
+      using var streamReaderA = new StreamReader(fileStreamA, Encoding.UTF8);
+
+      while (!streamReaderA.EndOfStream)
+      {
+        string line = streamReaderA.ReadLine();
+        Assert.That(line,
+          Is.EqualTo("[Info   :   loggerA] Info")
+            .Or.EqualTo("[Debug  :   loggerA] Debug")
+            .Or.EqualTo("[Message:   loggerA] Message")
+            .Or.EqualTo("[Error  :   loggerA] Error")
+            .Or.EqualTo("[Fatal  :   loggerA] Fatal")
+            .Or.EqualTo("[Warning:   loggerA] Warning")
+            .Or.EqualTo("[All    :   loggerA] Trace")
+        );
+      }
+
+      FileInfo logFileInfoB = new FileInfo("loggerB.Trace.log");
+      System.Console.WriteLine(logFileInfoB.FullName);
+      Assert.That(logFileInfoB, Is.Not.Null);
+      Assert.That(logFileInfoB.Exists, Is.True);
+
+      using var fileStreamB = new FileStream(logFileInfoB.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 1024, FileOptions.None);
+      using var streamReaderB = new StreamReader(fileStreamB, Encoding.UTF8);
+
+      while (!streamReaderB.EndOfStream)
+      {
+        string line = streamReaderB.ReadLine();
+        Assert.That(line,
+          Is.EqualTo("[Info   :   loggerB] Info")
+            .Or.EqualTo("[Debug  :   loggerB] Debug")
+            .Or.EqualTo("[Message:   loggerB] Message")
+            .Or.EqualTo("[Error  :   loggerB] Error")
+            .Or.EqualTo("[Fatal  :   loggerB] Fatal")
+            .Or.EqualTo("[Warning:   loggerB] Warning")
+            .Or.EqualTo("[All    :   loggerB] Trace")
+        );
+      }
+
+      FileInfo logFileInfoC = new FileInfo("loggerC.Trace.log");
+      System.Console.WriteLine(logFileInfoC.FullName);
+      Assert.That(logFileInfoC, Is.Not.Null);
+      Assert.That(logFileInfoC.Exists, Is.True);
+
+      using var fileStreamC = new FileStream(logFileInfoC.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 1024, FileOptions.None);
+      using var streamReaderC = new StreamReader(fileStreamC, Encoding.UTF8);
+
+      while (!streamReaderC.EndOfStream)
+      {
+        string line = streamReaderC.ReadLine();
+        Assert.That(line,
+          Is.EqualTo("[Info   :   loggerC] Info")
+            .Or.EqualTo("[Debug  :   loggerC] Debug")
+            .Or.EqualTo("[Message:   loggerC] Message")
+            .Or.EqualTo("[Error  :   loggerC] Error")
+            .Or.EqualTo("[Fatal  :   loggerC] Fatal")
+            .Or.EqualTo("[Warning:   loggerC] Warning")
+            .Or.EqualTo("[All    :   loggerC] Trace")
         );
       }
     }
